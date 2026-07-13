69981fbc0ad021d2179517a4c07e0e945e1f9699
sin005090 <sin005090@gmail.com>
2026-07-13 14:46:00 +0900

    나이 구하기

---
+++ src/main/java/kopo/programmers/level0/Age.java
@@ -0,0 +1,22 @@
+package kopo.programmers.level0;
+
+public class Age {
+    public static void main(String[] args) {
+
+        /*
+        머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다.
+         2022년 기준 선생님의 나이 age가 주어질 때,
+          선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
+
+         */
+    }
+    class solution {
+        public int solution(int age) {
+
+            int n1, solution;
+            n1 = 40;
+            solution = 2022 - n1 + 1;
+            return solution;
+        }
+    }
+}
