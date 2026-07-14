e08aba78dc0ea95d6eef1a9a4cdd08bae1211a9b
sin005090 <sin005090@gmail.com>
2026-07-14 16:48:53 +0900

    몫 구하기

---
+++ src/main/java/kopo/programmers/level0/Share.java
@@ -0,0 +1,20 @@
+package kopo.programmers.level0;
+
+public class Share {
+    public static void main(String[] args) {
+
+        /*
+        정수 num1, num2가 매개변수로 주어질 때,
+         num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+         */
+    }
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = 0;
+            num1 = 10;
+            num2 = 5;
+            answer = num1 / num2;
+            return answer;
+        }
+    }
+}
